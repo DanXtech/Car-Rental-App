8e1a221d47716fde9755053955811a1bbc7a601c
Danxtech <adetoyedaniel2023@gmail.com>
2023-12-16 01:15:23 -0800

    first commit

---
+++ src/components/Helmet/Helmet.jsx
@@ -0,0 +1,11 @@
+
+const Helmet = (props) => {
+  document.title = "  Rent Car Service - " + props.title
+  return (
+    <>
+      <div className="w-100">{props.children}</div>
+    </>
+  )
+}
+
+export default Helmet
